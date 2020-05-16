7488f096148947da929cf5da7e36ddc704aee0e8
Beatriz Milz <beatriz.milz@hotmail.com>
2020-05-16 17:50:47 -0300

    agora tem um render separado para pessoas monitoras

--- certificados/render_certificados.R
+++ certificados/render_certificados.R
@@ -2,22 +2,39 @@ library(readxl)
 library(dplyr)
 library(janitor)
 library(tidyverse)
+library(readr)
 
 # abrir a lista de presenca
 
-lista_de_presenca <-
-  read_excel(
-    "certificados/lista-preenchida.xlsx",
-    col_types = c("skip", "text", "skip",
-                  "text", "text")
+#lista_de_presenca <-
+  # read_excel(
+  #   "certificados/lista-preenchida.xlsx",
+  #   col_types = c("skip", "text", "skip",
+  #                 "text", "text")
+  # ) %>% janitor::clean_names() %>%
+  # filter(presenca == "presente")
+
+
+  lista_de_presenca <-
+  read_delim(
+    "certificados/meetup-16-05-2020.csv",
+    ";",
+    escape_double = FALSE,
+    col_types = cols(
+      `Respondeu ao RSVP em` = col_skip(),
+      X9 = col_skip(),
+      ordem = col_skip()
+    ),
+    trim_ws = TRUE
   ) %>% janitor::clean_names() %>%
-  filter(presenca == "presente")
+     filter(presenca == TRUE)
 
 
+# Organizadoras --------------------------------------
 # Organizadoras - Ler arquivos
 
 lista_de_presenca_organizadoras <-
-  lista_de_presenca %>% filter(observacao == "Organização") %>%
+  lista_de_presenca %>% filter(observacao == "organizador(a)") %>%
   select(nome_completo) %>%
   as_vector()
 
@@ -31,12 +48,27 @@ purrr::walk(
   )
 )
 
-# ------------
+# Monitoras --------------------------------------
+lista_de_presenca_monitoria <-
+  lista_de_presenca %>% filter(observacao == "monitor(a)") %>%
+  select(nome_completo) %>%
+  as_vector()
 
+#  gerar certificados em html
+purrr::walk(
+  lista_de_presenca_monitoria,
+  ~ rmarkdown::render(
+    "certificados/modelo_certificados.Rmd",
+    params = list(nome_participante = ., tipo_participacao = "monitora(o)"),
+    output_file = glue::glue('Certificado_Monitoria_R-Ladies_{.}.html')
+  )
+)
+
+# Participantes --------------------------------------
 # Participantes - ler arquivo
 
 lista_de_presenca_participantes <-
-  lista_de_presenca %>% filter(observacao == "Participante") %>%
+  lista_de_presenca %>% filter(observacao == "participante") %>%
   select(nome_completo) %>%
   as_vector()
 
